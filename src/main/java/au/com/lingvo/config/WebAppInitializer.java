bab23dc621986561714e881fc5858a77f1367e57
aleuchanka <anatoli.leuchanka@gmail.com>
2015-05-02 20:09:35 +1000

    links works

--- src/main/java/au/com/lingvo/config/WebAppInitializer.java
+++ src/main/java/au/com/lingvo/config/WebAppInitializer.java
@@ -2,7 +2,9 @@ package au.com.lingvo.config;
 
 import au.com.lingvo.filters.SimpleCORSFilter;
 import org.springframework.data.rest.webmvc.RepositoryRestDispatcherServlet;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import javax.servlet.ServletContext;
@@ -12,10 +14,14 @@ import javax.servlet.ServletRegistration.Dynamic;
 public class WebAppInitializer implements WebApplicationInitializer {
 
     public void onStartup(final ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(AppConfig.class);
-        ctx.setServletContext(servletContext);
-        Dynamic dynamic = servletContext.addServlet("dispatcher", new RepositoryRestDispatcherServlet(ctx));
+        AnnotationConfigWebApplicationContext rootCtx = new AnnotationConfigWebApplicationContext();
+        rootCtx.register(AppConfig.class);
+
+        servletContext.addListener(new ContextLoaderListener(rootCtx));
+
+        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+        webCtx.register(RepositoryRestMvcConfiguration.class);
+        Dynamic dynamic = servletContext.addServlet("dispatcher", new RepositoryRestDispatcherServlet(webCtx));
         dynamic.addMapping("/*");
         dynamic.setLoadOnStartup(1);
 
