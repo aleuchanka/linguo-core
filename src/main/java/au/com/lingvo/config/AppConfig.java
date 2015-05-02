8021a962bf4f3abf0e55f5d0d890a6d63b06f535
aleuchanka <anatoli.leuchanka@gmail.com>
2015-05-02 21:23:19 +1000

    add setup of dummy data

--- src/main/java/au/com/lingvo/config/AppConfig.java
+++ src/main/java/au/com/lingvo/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJpaRepositories(basePackages = "au.com.lingvo")
 @Import(RepositoryRestMvcConfiguration.class)
+@EnableScheduling
 public class AppConfig extends RepositoryRestMvcConfiguration {
 
 }
