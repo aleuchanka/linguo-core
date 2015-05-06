3cfc68f2a68361bf214de941b859a9b2f6ff178c
aleuchanka <anatoli.leuchanka@gmail.com>
2015-05-06 19:03:26 +1000

    simplify

--- src/main/java/au/com/lingvo/config/AppConfig.java
+++ src/main/java/au/com/lingvo/config/AppConfig.java
@@ -1,19 +1,23 @@
 package au.com.lingvo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("au.com.lingvo")
 @EnableWebMvc
-@EnableJpaRepositories(basePackages = "au.com.lingvo")
 @Import(RepositoryRestMvcConfiguration.class)
 @EnableScheduling
-public class AppConfig extends RepositoryRestMvcConfiguration {
+public class AppConfig {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
